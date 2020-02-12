9119156d7bc839425bc2db3401015f81780268e8
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2020-02-12 13:55:49 -0800

    logo added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Navbar, Nav, Button } from 'react-bootstrap';
 import './NavBar.scss';
+import Logo from './Logo'
 
 const NavBar = (props) => {
 
   return (
     <Navbar className='nav' expand="lg">
+      <Logo />
       <Navbar.Brand className='brand' href="#home">Catify</Navbar.Brand>
       <Nav className="mr-auto">
         <Nav.Link style={{ color: '#7A6181' }} href="#home">All Cats</Nav.Link>
