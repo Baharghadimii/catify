e6fcf25b18271e225f888a21b14d6db3791a8e53
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2020-02-12 16:36:47 -0800

    item  added

--- src/components/Cat.js
+++ src/components/Cat.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import './Cat.scss';
 
 const Cat = () => {
   return (
-    <div>Cat</div>
+    <div className='item'>
+      <h1 className='itm-txt'>Abyssinian</h1>
+      <div className='div-desc'>
+        <p className='itm-desc'>
+          The Abyssinian is easy to care for, and a joy to have in your home. They’re affectionate cats and love both people and other animals.
+      </p>
+      </div>
+      <button className='btn-itm'>Add to list</button>
+    </div>
   )
 }
 
