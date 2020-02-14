3d33b8ead46855382547f16189327dd67159a1f1
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2020-02-13 19:50:27 -0800

    handle displaying check and spinner

--- src/components/Cat.js
+++ src/components/Cat.js
@@ -6,56 +6,83 @@ const Cat = (props) => {
 
   const [modal, setModal] = useState({
     open: false
+  });
+  const [spinner, setSpinner] = useState({
+    opshowen: false
+  });
+  const [check, setCheck] = useState({
+    show: false
   })
 
   const handleAddToList = () => {
     setModal({
       open: true
     });
+    setSpinner({
+      show: true
+    })
     setTimeout(() => {
+      setSpinner({
+        show: false
+      });
+      setCheck({
+        show: true
+      })
+    }, 3000)
+    setTimeout(() => {
+      setCheck({
+        show: false
+      });
       setModal({
         open: false
       })
-    }, 3000)
+    }, 5000);
   }
   return (
     <div>
       {
         modal.open &&
         <div className="spinner-div">
-          <span style={{ color: 'white' }}><FaCheck size={50} /></span>
-          <div class="sk-circle">
-            <div class="sk-circle1 sk-child"></div>
-            <div class="sk-circle2 sk-child"></div>
-            <div class="sk-circle3 sk-child"></div>
-            <div class="sk-circle4 sk-child"></div>
-            <div class="sk-circle5 sk-child"></div>
-            <div class="sk-circle6 sk-child"></div>
-            <div class="sk-circle7 sk-child"></div>
-            <div class="sk-circle8 sk-child"></div>
-            <div class="sk-circle9 sk-child"></div>
-            <div class="sk-circle10 sk-child"></div>
-            <div class="sk-circle11 sk-child"></div>
-            <div class="sk-circle12 sk-child"></div>
-          </div>
-          <h4 className="spinner-txt">Adding...</h4>
+          {check.show && <div className="div-check">
+            < span style={{ color: 'white' }}><FaCheck size={50} /></span>
+            <h4 className="spinner-txt success">Added to your List</h4>
+          </div>}
+          {spinner.show && <div>
+            < div class="sk-circle">
+              <div class="sk-circle1 sk-child"></div>
+              <div class="sk-circle2 sk-child"></div>
+              <div class="sk-circle3 sk-child"></div>
+              <div class="sk-circle4 sk-child"></div>
+              <div class="sk-circle5 sk-child"></div>
+              <div class="sk-circle6 sk-child"></div>
+              <div class="sk-circle7 sk-child"></div>
+              <div class="sk-circle8 sk-child"></div>
+              <div class="sk-circle9 sk-child"></div>
+              <div class="sk-circle10 sk-child"></div>
+              <div class="sk-circle11 sk-child"></div>
+              <div class="sk-circle12 sk-child"></div>
+            </div>
+            <h4 className="spinner-txt">Adding...</h4>
+          </div>}
         </div>
       }
-      {!modal.open && < div className='item-card' >
-        <div className='div-txt'>
-          <h1 className='itm-txt'>{props.cat.name}</h1>
-          <div className='div-desc'>
-            <p className='itm-desc'>
-              {props.cat.description}
-            </p>
+      {
+        !modal.open && < div className='item-card' >
+          <div className='div-txt'>
+            <h1 className='itm-txt'>{props.cat.name}</h1>
+            <div className='div-desc'>
+              <p className='itm-desc'>
+                {props.cat.description}
+              </p>
+            </div>
+            <button className='btn-itm' onClick={handleAddToList}>Add to list</button>
           </div>
-          <button className='btn-itm' onClick={handleAddToList}>Add to list</button>
-        </div>
-        <div className='pic-div'>
-          <img className='pic' alt='' src={props.cat.image}></img>
-        </div>
-      </div >}
-    </div>
+          <div className='pic-div'>
+            <img className='pic' alt='' src={props.cat.image}></img>
+          </div>
+        </div >
+      }
+    </div >
 
   )
 }
