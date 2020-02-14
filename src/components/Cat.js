5c00465b0019915ba244be9faae8af3650677f86
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2020-02-13 18:57:34 -0800

    container for spinner added

--- src/components/Cat.js
+++ src/components/Cat.js
@@ -1,22 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Cat.scss';
 
 const Cat = (props) => {
+
+  const [modal, setModal] = useState({
+    open: false
+  })
+
+  const handleAddToList = () => {
+    setModal({
+      open: true
+    })
+  }
   return (
-    <div className='item-card'>
-      <div className='div-txt'>
-        <h1 className='itm-txt'>{props.cat.name}</h1>
-        <div className='div-desc'>
-          <p className='itm-desc'>
-            {props.cat.description}
-          </p>
+    <div>
+      {
+        modal.open && <div className="spinner-div"></div>
+      }
+      {!modal.open && < div className='item-card' >
+        <div className='div-txt'>
+          <h1 className='itm-txt'>{props.cat.name}</h1>
+          <div className='div-desc'>
+            <p className='itm-desc'>
+              {props.cat.description}
+            </p>
+          </div>
+          <button className='btn-itm' onClick={handleAddToList}>Add to list</button>
         </div>
-        <button className='btn-itm'>Add to list</button>
-      </div>
-      <div className='pic-div'>
-        <img className='pic' alt='' src={props.cat.image}></img>
-      </div>
+        <div className='pic-div'>
+          <img className='pic' alt='' src={props.cat.image}></img>
+        </div>
+      </div >}
     </div>
+
   )
 }
 
