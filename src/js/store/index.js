2cac019bda5e10840b2a16c2abf73507cdd1178a
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2020-02-15 17:56:06 -0800

    refactor index

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "../reducers/index";
 import createSagaMiddleware from "redux-saga";
-import apiSaga from "../sagas/api-saga";
+import apiSaga from "../saga/api-saga";
 
 const initialiseSagaMiddleware = createSagaMiddleware();
 
